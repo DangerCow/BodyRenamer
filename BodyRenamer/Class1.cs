100567d6f0a57e40aca6c3d079b42c78c45ef610
DangerCow <38820244+DangerCow@users.noreply.github.com>
2019-01-28 18:21:51 -0700

    update 0.1.0

---
+++ BodyRenamer/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BodyRenamer
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Hello : MonoBehaviour
+    {
+        [KSPField]
+        public string names;
+
+        public string body_name = "seronimo waz here";
+        private CelestialBody[] bodys;
+
+        public void Start()
+        {
+            print("hello!");
+            print(names);
+            bodys = FindObjectsOfType(typeof(CelestialBody)) as CelestialBody[];
+
+            foreach (CelestialBody body in bodys)
+            {
+                body.bodyDisplayName = body_name;
+                body.bodyAdjectiveDisplayName = body_name;
+            }
+        }
+    }
+}
