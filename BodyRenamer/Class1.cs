89e304ea872da1e92c8ab6fe91b998a33ccb2a08
DangerCow <38820244+DangerCow@users.noreply.github.com>
2019-01-28 18:30:35 -0700

    Update Class1.cs

--- BodyRenamer/Class1.cs
+++ BodyRenamer/Class1.cs
@@ -9,16 +9,11 @@ namespace BodyRenamer
     [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     public class Hello : MonoBehaviour
     {
-        [KSPField]
-        public string names;
-
         public string body_name = "seronimo waz here";
         private CelestialBody[] bodys;
 
         public void Start()
         {
-            print("hello!");
-            print(names);
             bodys = FindObjectsOfType(typeof(CelestialBody)) as CelestialBody[];
 
             foreach (CelestialBody body in bodys)
